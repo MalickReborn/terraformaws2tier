f155e409aa8d7d2aef2d152a3b347f4ef518bdad
MalickReborn <105162202+MalickReborn@users.noreply.github.com>
2025-03-22 15:24:21 +0000

    Update vpc

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,14 @@
-resource "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
+resource "aws_vpc" "my-vpc" {
+  #source = "terraform-aws-modules/vpc/aws"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
   /* azs             = ["eu-west-1a", "eu-west-1b", ]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"] */
-
-  enable_nat_gateway = true
   enable_dns_hostnames = true
 
   tags = {
-    Terraform = "true"
+    name = "my-vpc"
     Environment = "dev"
   }
 }
