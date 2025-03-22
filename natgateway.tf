20bf5a94ab3262d9be55af0374c5fd271c2798e5
MalickReborn <105162202+MalickReborn@users.noreply.github.com>
2025-03-22 16:07:44 +0000

    Update natgateway.tf

--- natgateway.tf
+++ natgateway.tf
@@ -1,25 +1,27 @@
 resource "aws_nat_gateway" "natgateway1" {
-  allocation_id = aws_eip.example.id
+  allocation_id = aws_eip.nat1.id
   subnet_id     = aws_subnet.public1
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
 
   tags = {
     Name = "natgateway1"
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_nat_gateway" "natgateway2" {
-  allocation_id = aws_eip.example.id
+  allocation_id = aws_eip.nat2.id
   subnet_id     = aws_subnet.public2
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
 
   tags = {
     Name = "natgateway2"
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw]
 }
